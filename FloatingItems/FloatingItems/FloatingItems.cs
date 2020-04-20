56eb72b934c6f75810f52c2af75121aa48e550df
Brian Zulch <lego.trains.b@gmail.com>
2020-04-19 21:19:27 -0700

    added project files

---
+++ FloatingItems/FloatingItems/FloatingItems.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EXILED;
+using Harmony;
+
+namespace FloatingItems
+{
+    public class FloatingItems : Plugin
+    {
+        public override string getName => "FloatingItems";
+        public HarmonyInstance inst;
+        public static float minForce;
+        public static float maxForce;
+        public static float maxOffset;
+
+        public override void OnDisable()
+        {
+            if (!Config.GetBool("floating_items"))
+                return;
+            inst.UnpatchAll();
+        }
+
+        public override void OnEnable()
+        {
+            if (!Config.GetBool("floating_items"))
+                return;
+            minForce = Config.GetFloat("fitem_force_min", 5.0f);
+            maxForce = Config.GetFloat("fitem_force_max", 10.0f);
+            maxOffset = Config.GetFloat("fitem_offset_max", 2.0f);
+            inst = HarmonyInstance.Create("virtualbrightplayz.exiledfloatingitems");
+            inst.PatchAll();
+        }
+
+        public override void OnReload()
+        {
+            if (!Config.GetBool("floating_items"))
+                return;
+            minForce = Config.GetFloat("fitem_force_min", 5.0f);
+            maxForce = Config.GetFloat("fitem_force_max", 10.0f);
+            maxOffset = Config.GetFloat("fitem_offset_max", 2.0f);
+            inst = HarmonyInstance.Create("virtualbrightplayz.exiledfloatingitems");
+            inst.PatchAll();
+        }
+    }
+}
---
+++ FloatingItems/FloatingItems/PickupPatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+
+namespace FloatingItems
+{
+    [HarmonyPatch(typeof(Pickup), nameof(Pickup.LateUpdate))]
+    public class PickupUpdatePatch
+    {
+        public static List<Pickup> haveBeenMoved = new List<Pickup>();
+
+        public static void Postfix(Pickup __instance)
+        {
+            __instance.Rb.useGravity = false;
+            __instance.GetComponent<Collider>().material = new PhysicMaterial() { bounciness = 100f, bounceCombine = PhysicMaterialCombine.Maximum };
+            if (haveBeenMoved.Contains(__instance))
+                return;
+            float p1 = UnityEngine.Random.Range(-FloatingItems.maxOffset, FloatingItems.maxOffset);
+            float p2 = UnityEngine.Random.Range(-FloatingItems.maxOffset, FloatingItems.maxOffset);
+            float p3 = UnityEngine.Random.Range(-FloatingItems.maxOffset, FloatingItems.maxOffset);
+            Vector3 v3 = new Vector3(p1, p2, p3);
+            float rng = UnityEngine.Random.Range(FloatingItems.minForce, FloatingItems.maxForce);
+            __instance.Rb.AddExplosionForce(rng, __instance.Rb.position + v3, 10f);
+            haveBeenMoved.Add(__instance);
+        }
+    }
+
+    /*[HarmonyPatch(typeof(Pickup), nameof(Pickup.Start))]
+    public class PickupStartPatch
+    {
+        public static void Postfix(Pickup __instance)
+        {
+            //__instance.Rb.useGravity = false;
+            float p1 = UnityEngine.Random.Range(-0.5f, 0.5f);
+            float p2 = UnityEngine.Random.Range(-0.5f, 0.5f);
+            float p3 = UnityEngine.Random.Range(-0.5f, 0.5f);
+            Vector3 v3 = new Vector3(p1, p2, p3);
+            float rng = UnityEngine.Random.Range(0.1f, 0.5f);
+            __instance.Rb.AddExplosionForce(rng, __instance.Rb.position + v3, 10f);
+        }
+    }*/
+}
